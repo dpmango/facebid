3a280493ef553917516d610a1c43138e8c9ca301
Sergey Khmelevskoy <xs290@me.com>
2018-08-26 19:58:44 +0300

    added select

--- src/components/Events/Filters.js
+++ src/components/Events/Filters.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
 import SvgIcon from '../../components/Helpers/SvgIcon';
 import FilterSlider from './FilterSlider';
+// import Select from '../Forms/Select';
+import Select from 'react-select';
 
 class Filters extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      gender: null
+    }
+  }
+
   createEventClick = () => {
 
   }
@@ -11,7 +21,18 @@ class Filters extends Component {
 
   }
 
+  handleSelectChange = (value, name) => {
+    this.setState({ [name]: value })
+  }
+
   render(){
+
+    const {
+      state: {
+        gender
+      }
+    } = this
+
     return(
       <div className="filters">
         <div className="filters__top">
@@ -31,12 +52,22 @@ class Filters extends Component {
           <div className="filters__options-col filters__options-col--gender">
             <div className="ui-group">
               <label htmlFor="">Показывать</label>
-              <div className="ui-input">
-                <input type="text"/>
-              </div>
+              <Select
+                className="Select-main"
+                name="gender"
+                searchable={false}
+                autosize={false}
+                value={gender}
+                onChange={(e) => this.handleSelectChange(e, "gender")}
+                placeholder="Выберите пол"
+                options={[
+                  { value: 'female', label: 'Девушек' },
+                  { value: 'male', label: 'Парней' }
+                ]}
+              />
             </div>
           </div>
-          <div className="filters__options-col filters__options-col--gender">
+          <div className="filters__options-col filters__options-col--radius">
             <div className="ui-group">
               <label htmlFor="">В радиусе</label>
               <div className="ui-input">
@@ -44,7 +75,7 @@ class Filters extends Component {
               </div>
             </div>
           </div>
-          <div className="filters__options-col filters__options-col--gender">
+          <div className="filters__options-col filters__options-col--age">
             <div className="ui-group">
               <label htmlFor="">Возраст</label>
               <div className="ui-input">
@@ -52,13 +83,16 @@ class Filters extends Component {
               </div>
             </div>
           </div>
-          <div className="filters__options-col filters__options-col--gender">
+          <div className="filters__options-col filters__options-col--languages">
             <div className="ui-group">
               <label htmlFor="">Языки</label>
               <div className="ui-input">
                 <input type="text"/>
               </div>
             </div>
+          </div>
+          <div className="filters__options-col filters__options-col--clear">
+            <a href="#" className="t-link-small">Очистить</a>
           </div>
         </div>
         <div className="filters__categories">
---
+++ src/components/Forms/Select.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Select extends Component {
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default Select
