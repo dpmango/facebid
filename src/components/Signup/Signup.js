b8e64f9b5e5a532abe61b32116c6cd7b6ca753a0
Sergey Khmelevskoy <xs290@me.com>
2018-08-30 00:14:12 +0300

    added some signup fields

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import Formsy from 'formsy-react';
 import { connect } from 'react-redux';
+import Select from 'react-select';
 import Modal from '../Modal/Modal';
 import FormInput from '../Forms/Input';
+import Toggle from '../Forms/Toggle';
 import SocialLogin from './SocialLogin';
 
 class Signup extends Component{
   constructor(props){
     super(props)
     this.state = {
-      modalOpened: false,
+      modalOpened: true,
       nickname: '',
+      birth_day: '',
+      birth_month: '',
+      birth_year: '',
+      gender: null,
+      city: '',
       email: '',
       password: '',
       password_repeat: ''
@@ -70,6 +77,25 @@ class Signup extends Component{
     }
   }
 
+  // select functions
+  handleSelectChange = (value, name) => {
+    this.setState({ [name]: value })
+  }
+
+  mapArrToSelect = (arr) => {
+    // create react-select value/label pair from arr entiries
+    return arr.map(el => {
+      return { value: el, label: el }
+    })
+  }
+
+  // toggle functions
+  selectToggle = (val) => {
+    this.setState({
+      gender: val
+    })
+  }
+
   signupUser = () => {
     // auth
     alert('TODO - auth')
@@ -78,7 +104,9 @@ class Signup extends Component{
   render(){
     const {
       state: {
-        nickname, email, password, password_repeat
+        nickname,
+        birth_day, birth_month, birth_year,
+        gender, city, email, password, password_repeat
       }
     } = this
 
@@ -116,6 +144,92 @@ class Signup extends Component{
                     onChangeHandler={this.handleChange}
                     onKeyHandler={this.keyPressHandler}
                     required />
+                  <div className="ui-group ui-group--row">
+                    <label htmlFor="">Дата рождения</label>
+                    <div className="ui-birth-selects">
+                      <Select
+                        name="birth_day"
+                        searchable={false}
+                        autosize={false}
+                        value={birth_day}
+                        onChange={(e) => this.handleSelectChange(e, "birth_day")}
+                        placeholder="день"
+                        options={this.mapArrToSelect(
+                          [
+                            "01", "02", "03", "04", "05", "06", "07", "08", "09",
+                            "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                            "20", "21", "22", "23", "24", "25", "26", "27", "28", "29"
+                          ]
+                        )
+                        }
+                      />
+                      <Select
+                        name="birth_month"
+                        searchable={false}
+                        autosize={false}
+                        value={birth_month}
+                        onChange={(e) => this.handleSelectChange(e, "birth_month")}
+                        placeholder="месяц"
+                        options={[
+                          {value: "01", label: "Января"},
+                          {value: "02", label: "Ферваля"},
+                          {value: "03", label: "Марта"},
+                          {value: "04", label: "Апреля"},
+                          {value: "05", label: "Мая"},
+                          {value: "06", label: "Июня"},
+                          {value: "07", label: "Июля"},
+                          {value: "08", label: "Августа"},
+                          {value: "09", label: "Сентября"},
+                          {value: "10", label: "Октября"},
+                          {value: "11", label: "Ноября"},
+                          {value: "12", label: "Декабря"}
+                        ]}
+                      />
+                      <Select
+                        name="birth_year"
+                        searchable={false}
+                        autosize={false}
+                        value={birth_year}
+                        onChange={(e) => this.handleSelectChange(e, "birth_year")}
+                        placeholder="год"
+                        options={this.mapArrToSelect(
+                          [
+                            "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001",
+                            "2000", "1999", "1998", "1997", "1996", "1995", "1994", "1993", "1992", "1991",
+                            "1990", "1989", "1988", "1987", "1986", "1985", "1984", "1983", "1982", "1981"
+                          ]
+                        )
+                        }
+                      />
+                    </div>
+                  </div>
+                  <div className="ui-group ui-group--row">
+                    <label htmlFor="">Пол</label>
+                    <Toggle
+                      value={gender}
+                      options={{
+                        left: "Женский",
+                        right: "Мужской"
+                      }}
+                      clickHandler={this.selectToggle} />
+                  </div>
+                  <div className="ui-group ui-group--row">
+                    <label htmlFor="">Город</label>
+                    <Select
+                      name="city"
+                      searchable={false}
+                      autosize={false}
+                      value={city}
+                      onChange={(e) => this.handleSelectChange(e, "city")}
+                      placeholder="Укажите местоположение"
+                      options={this.mapArrToSelect(
+                        [
+                          "Москва", "Санкт-Питербург"
+                        ]
+                      )
+                      }
+                    />
+                  </div>
                   <FormInput
                     name="email"
                     label="Email:"
