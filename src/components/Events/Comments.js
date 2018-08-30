a75d325a9496f46ad6da55bd8c139f36fa9cbd06
Sergey Khmelevskoy <xs290@me.com>
2018-08-30 20:50:15 +0300

    added scrollbar, refactored comments components

--- src/components/Events/Comments.js
+++ src/components/Events/Comments.js
@@ -3,24 +3,45 @@ import Image from '../Helpers/Image';
 
 class Comments extends Component {
   render(){
+    const comments = [
+      {
+        id: 1,
+        user: {
+          avatar: "userAvatar_3.jpg",
+          name: "Susie Holt"
+        },
+        text: "Супер! Я бы тоже не против скататься в такое интересное путешествие. Когда выезжаете?"
+      },
+      {
+        id: 2,
+        user: {
+          avatar: "userAvatar_2.jpg",
+          name: "Hello World"
+        },
+        text: "Супер! Я бы тоже не против скататься в такое интересное путешествие. Когда выезжаете?"
+      }
+    ]
     return(
       <div className="e-card__comments">
-        <div className="e-comment">
-          <div className="avatar">
-            <Image file="userAvatar_3.jpg" />
-          </div>
-          <div className="e-comment__contents">
-            <div className="e-comment__top">
-              <div className="e-comment__name">Susie Holt</div>
-              <div className="e-comment__actions">
-
+        {comments.map(comment => {
+          return(
+            <div className="e-comment" key={comment.id}>
+              <div className="avatar">
+                <Image file={comment.user.avatar} />
+              </div>
+              <div className="e-comment__contents">
+                <div className="e-comment__top">
+                  <div className="e-comment__name">{comment.user.name}</div>
+                  <div className="e-comment__actions">
+                  </div>
+                </div>
+                <div className="e-comment__text">
+                  {comment.text}
+                </div>
               </div>
             </div>
-            <div className="e-comment__text">
-              Супер! Я бы тоже не против скататься в такое интересное путешествие. Когда выезжаете?
-            </div>
-          </div>
-        </div>
+          )
+        })}
       </div>
     )
   }
--- src/components/Events/EventCard.js
+++ src/components/Events/EventCard.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
 import Swiper from 'react-id-swiper';
+import PerfectScrollbar from 'react-perfect-scrollbar';
 import SvgIcon from '../Helpers/SvgIcon';
 import Image from '../Helpers/Image';
 import ConvertMonthNumToName from '../../services/ConvertMonthNumToName';
 import AvatarList from '../People/AvatarList';
 import Comments from './Comments';
 
+// TODO
+import 'react-perfect-scrollbar/dist/css/styles.css';
+
 class EventCard extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      shouldCtaStick: false
+    }
+
+    this.ctaRef = React.createRef();
+
+  }
+
   convertDate = (str) => {
     const date = str.substring(0,2)
     const month = ConvertMonthNumToName(str.substring(2))
@@ -17,6 +33,18 @@ class EventCard extends Component {
         <span>{month}</span>
       </React.Fragment>
     )
+  }
+
+  onScroll = (container) => {
+    console.log(container.scrollTop);
+
+    console.log('cta ref', this.ctaRef.current)
+
+    // if ( bla bla ){
+    //   this.setState({ shouldCtaStick: true })
+    // } else {
+    //   this.setState({ shouldCtaStick: false })
+    // }
   }
 
   render(){
@@ -79,61 +107,75 @@ class EventCard extends Component {
             </Swiper>
           </div>
           <div className="e-card__contents">
-            <div className="e-card__top">
-              <div className="e-card__user">
-                <div className="e-card__user-avatar">
-                  <Image file="userAvatar.jpg" />
+            <div className="e-card__contents-wrapper">
+              {/* top */}
+              <div className="e-card__top">
+                <div className="e-card__user">
+                  <div className="e-card__user-avatar">
+                    <Image file="userAvatar.jpg" />
+                  </div>
+                  <div className="e-card__user-info">
+                    <div className="e-card__user-line">
+                      <div className="e-card__user-name">{user.name}, {user.age}</div>
+                      <div className="e-card__user-status">
+                        {user.isVerified &&
+                          <div className="icon-verified">
+                            <SvgIcon name="checkmark" />
+                          </div>
+                        }
+                      </div>
+                    </div>
+                    <div className="e-card__user-distance">{user.distance}</div>
+                  </div>
                 </div>
-                <div className="e-card__user-info">
-                  <div className="e-card__user-line">
-                    <div className="e-card__user-name">{user.name}, {user.age}</div>
-                    <div className="e-card__user-status">
-                      {user.isVerified &&
-                        <div className="icon-verified">
-                          <SvgIcon name="checkmark" />
-                        </div>
-                      }
+                <div className="e-card__actions">
+                  <div className="e-card__action e-card__bookmark">
+                    <SvgIcon name="bookmark" />
+                  </div>
+                  <div className="e-card__action e-card__share">
+                    <SvgIcon name="share" />
+                  </div>
+                  <div className="e-card__action e-card__more">
+                    <SvgIcon name="more" />
+                  </div>
+                </div>
+              </div>
+              {/* scrollbale */}
+              <PerfectScrollbar
+                option={{
+                  wheelSpeed: 1,
+                  wheelPropagation: false,
+                  suppressScrollX: true,
+                }}
+                onScrollY={this.onScroll}
+                >
+                <div className="e-card__scrollable">
+                  <div className="e-card__head">
+                    <div className="e-card__title">{name}</div>
+                    <div className="e-card__event-line">
+                      <span>{from}</span>
+                      <i className="icon icon-plane"></i>
+                      <span>{to}</span>
+                    </div>
+                    <div className="e-card__date">
+                      {this.convertDate(date)}
                     </div>
                   </div>
-                  <div className="e-card__user-distance">{user.distance}</div>
+                  <div className="e-card__desc">{desc}</div>
+                  {/* cta (sticky on scroll) */}
+                  <div className="e-card__cta" ref={this.ctaRef}>
+                    <button className="btn btn-primary btn--iconed">
+                      <SvgIcon name="checkmark" />
+                      <span>Участвовать</span>
+                    </button>
+                    <div className="e-card__partisipants">
+                      <AvatarList
+                        avatars={avatars} />
+                    </div>
+                  </div>
+                  <Comments />
                 </div>
-              </div>
-              <div className="e-card__actions">
-                <div className="e-card__action e-card__bookmark">
-                  <SvgIcon name="bookmark" />
-                </div>
-                <div className="e-card__action e-card__share">
-                  <SvgIcon name="share" />
-                </div>
-                <div className="e-card__action e-card__more">
-                  <SvgIcon name="more" />
-                </div>
-              </div>
-            </div>
-            <div className="e-card__scrollable">
-              <div className="e-card__head">
-                <div className="e-card__title">{name}</div>
-                <div className="e-card__event-line">
-                  <span>{from}</span>
-                  <i className="icon icon-plane"></i>
-                  <span>{to}</span>
-                </div>
-                <div className="e-card__date">
-                  {this.convertDate(date)}
-                </div>
-              </div>
-              <div className="e-card__desc">{desc}</div>
-              <div className="e-card__cta">
-                <button className="btn btn-primary btn--iconed">
-                  <SvgIcon name="checkmark" />
-                  <span>Участвовать</span>
-                </button>
-                <div className="e-card__partisipants">
-                  <AvatarList
-                    avatars={avatars} />
-                </div>
-              </div>
-              <Comments />
+              </PerfectScrollbar>
             </div>
           </div>
         </div>
